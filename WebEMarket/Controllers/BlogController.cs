49381c8035969e594b6473a6a1445636d232ff1a
Administrator <nguyenninhthuan00021102@gmail.com>
2023-03-04 21:08:26 +0700

    Add project files.

---
+++ WebEMarket/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebEMarket.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebEMarket/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using WebEMarket.Models;
+
+namespace WebEMarket.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly dbMarketsContext _context;
+
+        public ProductController(dbMarketsContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            //xem product thuoc Category nao va tra ve phan tu dau tien duoc tim thay
+            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
+            if(product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
